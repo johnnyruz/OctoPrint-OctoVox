0134f3472495a64157f4d47c7218af03a4c43526
John Ruzick <johnnyruzdev01@outlook.com>
2019-10-20 22:34:49 +0000

    Update compatibility control property

--- octoprint_octovox/__init__.py
+++ octoprint_octovox/__init__.py
@@ -144,7 +144,7 @@ class OctovoxPlugin(octoprint.plugin.StartupPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "OctoVox Plugin"
-__plugin_pythoncompat__ = ">2.7,<4"
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
